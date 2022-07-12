7ef658806f075e57a52295f4cd1a9041389376de
gabycode <86427446+gabycode@users.noreply.github.com>
2022-07-12 15:51:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ let emailForm = document.getElementById("email")
 
 emailForm.addEventListener("submit", e => {
     e.preventDefault()
-    console.log("submitted")
+    let ourFormData = new FormData(e.target)
+    let userEmailAddress = ourFormData.get("emailAddress")
+
+    let updatedHtml = `
+        <h2>Thank you for suscribing!</h2>
+        <p class="fine-print">We'll never share your information without your permission</p>
+        <p class="fine-print">You will receive announcements and launch deals sent to: ${userEmailAddress}</p>`  
+
+    let ourTextContent = document.getElementById("text-content")
+    ourTextContent.innerHTML = updatedHtml
 })
